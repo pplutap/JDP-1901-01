23f2757251c553dee98e9f35eaef4ad5a26de891
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2019-01-30 16:28:18 +0100

    Added first implementation of OrderController with OrderRepository, OrderDto and OrderMapper

--- src/main/java/com/kodilla/ecommercee/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/OrderController.java
@@ -1,4 +1,47 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/orders")
 public class OrderController {
+
+    //@Autowired
+    //private DbService service;
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    @GetMapping(value="getOrders")
+    public List<OrderDto> getOrders() {
+        return new ArrayList<>();
+    }
+
+    @GetMapping(value="getOrder/{id}")
+    public OrderDto getOrder(@PathVariable("id") Long orderId) {
+        return new OrderDto();
+    }
+
+    @PostMapping(value="addOrder", consumes = APPLICATION_JSON_VALUE)
+    public void addOrder(@RequestBody OrderDto orderDto) {
+
+    }
+
+    @PutMapping(value="updateOrder")
+    public void updateOrder(@RequestBody OrderDto orderDto) {
+
+    }
+
+    @DeleteMapping(value="deleteOrder/{id}")
+    public void deleteOrder(@PathVariable("id") Long orderId) {
+
+    }
+
 }
---
+++ src/main/java/com/kodilla/ecommercee/OrderMapper.java
@@ -0,0 +1,25 @@
+package com.kodilla.ecommercee;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class OrderMapper {
+
+    public GenericEntity mapToOrder(final OrderDto orderDto) {
+        return new GenericEntity();
+    }
+
+    public OrderDto mapToOrderDto(final GenericEntity entity) {
+        return new OrderDto();
+    }
+
+    public List<OrderDto> mapToOrderDtoList(final List<GenericEntity> orderList) {
+        return orderList.stream()
+                .map(e->new OrderDto(e.getId(), e.getValue()))
+                .collect(Collectors.toList());
+    }
+
+}
