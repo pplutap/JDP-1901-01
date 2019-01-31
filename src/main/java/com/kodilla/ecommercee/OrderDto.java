1e664992a913f6402348dcfeb613ac995c4a5080
Kacper Szubra <41995059+kszubra@users.noreply.github.com>
2019-01-31 12:28:04 +0100

    added no args constr by hand instead of lombok one

--- src/main/java/com/kodilla/ecommercee/OrderDto.java
+++ src/main/java/com/kodilla/ecommercee/OrderDto.java
@@ -1,13 +1,14 @@
 package com.kodilla.ecommercee;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 public class OrderDto {
     private Long id;
     private String value;
+
+    public OrderDto() {
+    }
 
     public OrderDto(Long id, String value) {
         this.id = id;
