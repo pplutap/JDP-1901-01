9667a7ae2e7105691ff202784fed0dc7a14bd794
aleksanderkot00@gmail.com <aleksanderkot00@gmail.com>
2019-02-01 10:56:41 +0100

    Zastapienie adnotacji RequestMapping adnotacjami z wybranymi metodami oraz usuniecie parametru consumes

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -1,13 +1,9 @@
 package com.kodilla.ecommercee;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("/products")
@@ -18,22 +14,22 @@ public class ProductController {
         return new ArrayList<>();
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "getProduct")
+    @GetMapping(value = "getProduct")
     public ProductDto getProduct( Long productId) {
         return new ProductDto(1L, "test name", "test description", 9.99, 2L);
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "createProduct", consumes = APPLICATION_JSON_VALUE)
+    @PostMapping(value = "createProduct")
     public void createProduct(ProductDto productDto) {
 
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "updateProduct")
+    @PutMapping(value = "updateProduct")
     public ProductDto updateProduct(ProductDto productDto) {
         return new ProductDto(2L, "test name 2", "test description 2", 99.99, 12L);
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "deleteProduct")
+    @DeleteMapping(value = "deleteProduct")
     public void deleteProduct(Long productId) {
 
     }
