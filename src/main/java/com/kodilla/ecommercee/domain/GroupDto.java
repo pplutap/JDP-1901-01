d44a8f230e12bfdfef4feb794dcbc319bd87698d
radion358 <radoslaw.dauksza@gmail.com>
2019-02-03 18:00:41 +0100

    Implementation of Group entity

--- src/main/java/com/kodilla/ecommercee/domain/GroupDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/GroupDto.java
@@ -1,16 +1,19 @@
 package com.kodilla.ecommercee.domain;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class GroupDto {
 
     private long id;
     private String name;
+    private Set<Product> productsInGroup = new HashSet<>();
 
-    public GroupDto(long id, String name) {
+    public GroupDto(long id, String name, Set<Product> productsInGroup) {
         this.id = id;
         this.name = name;
+        this.productsInGroup = productsInGroup;
     }
-
-    public GroupDto() {}
 
     public long getId() {
         return id;
@@ -18,6 +21,10 @@ public class GroupDto {
 
     public String getName() {
         return name;
+    }
+
+    public Set<Product> getProductsInGroup() {
+        return productsInGroup;
     }
 }
 
