bc41b82cb8fff8eb9388b8dd3ccd6b131a6f6df0
radion358 <radoslaw.dauksza@gmail.com>
2019-02-03 18:00:41 +0100

    Implementation of Group entity

--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -1,21 +1,58 @@
 package com.kodilla.ecommercee.domain;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "GROUPS")
 public class Group {
     private long id;
     private String name;
+    private Set<Product> productsInGroup = new HashSet<>();
 
-    public Group(long id, String name) {
+    public Group(long id, String name, Set<Product> productsInGroup) {
         this.id = id;
         this.name = name;
+        this.productsInGroup = productsInGroup;
     }
 
-    public Group(Group group) {}
+    public Group() {}
 
+    @Id
+    @NotNull
+    @GeneratedValue
+    @Column(name = "ID", unique = true)
     public long getId() {
         return id;
     }
 
+    @NotNull
+    @Column(name = "GROUP_NAME", unique = true)
     public String getName() {
         return name;
+    }
+
+    @OneToMany(
+            targetEntity = Product.class,
+            mappedBy = "productsInGroup",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    public Set<Product> getProductsInGroup() {
+        return productsInGroup;
+    }
+
+    private void setId(long id) {
+        this.id = id;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    private void setProductsInGroup(Set<Product> productsInGroup) {
+        this.productsInGroup = productsInGroup;
     }
 }
