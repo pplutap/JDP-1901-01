4326973324438d83cc4b8e2d739454eeceeda3dc
radion358 <radoslaw.dauksza@gmail.com>
2019-02-23 18:31:23 +0100

    CsvToBean

--- src/main/java/com/kodilla/ecommercee/controller/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/controller/ProductController.java
@@ -1,15 +1,23 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.domain.Product;
 import com.kodilla.ecommercee.domain.dto.ProductDto;
 import com.kodilla.ecommercee.mapper.ProductMapper;
 import com.kodilla.ecommercee.service.FileStorageService;
 import com.kodilla.ecommercee.service.ProductService;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.HeaderColumnNameTranslateMappingStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/products")
@@ -54,5 +62,31 @@ public class ProductController {
     public void uploadFile(@RequestParam("file") MultipartFile file) {
         Path filePath = storageService.storeFile(file);
         System.out.println(filePath);
+
+        Map<String, String> mapping = new HashMap<>();
+        mapping.put("Name", "name");
+        mapping.put("Description", "description");
+        mapping.put("Price", "price");
+        mapping.put("Group", "group");
+
+        HeaderColumnNameTranslateMappingStrategy<Product> strategy =
+                new HeaderColumnNameTranslateMappingStrategy<>();
+        strategy.setType(Product.class);
+        strategy.setColumnMapping(mapping);
+
+        CSVReader csvReader = null;
+        try {
+            csvReader = new CSVReader(new FileReader(filePath.toString()));
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        CsvToBean csvToBean = new CsvToBean();
+
+        List<Product> list = csvToBean.parse(strategy, csvReader);
+
+        for (Product e : list) {
+            System.out.println(e);
+        }
     }
 }
